7a1f3d642d6d80bf08d0efe349f44d2a5d560468
Suma-Bade <sumabade009@gmail.com>
2020-02-06 16:50:12 +0530

    third commit

---
+++ JavaScript/MyScript1.js
@@ -0,0 +1,12 @@
+﻿var userName = "rama";
+function display(name) {
+    alert("MyScript1.js:" + name);
+}
+display(userName);
+////(function () {
+////    var userName = "Rama";
+////    function display(name) {
+////        alert("MyScript1.js:" + name);
+////    }
+////    display(userName);
+////})();
---
+++ JavaScript/MyScript2.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    var userName = "Steves";
+    function display(name) {
+        alert("MyScript2.js:" + name);
+    }
+    display(userName);
+})();
